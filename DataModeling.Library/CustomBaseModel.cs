d87aa6468e53e3ab5a5504da96a581c92cef317a
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-11-01 10:53:18 +0300

    Adding the base data model
    A class to represent the base data model added to the data modeling library, with an implementation for validation functionality

---
+++ DataModeling.Library/CustomBaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataModeling.Library;
+
+public abstract class CustomBaseModel
+{
+    public Guid Id { get; init; }
+
+    public bool Validate()
+    {
+        var context = new ValidationContext(this, serviceProvider: null, items: null);
+        var results = new List<ValidationResult>();
+        var isValid = Validator.TryValidateObject(this, context, results, true);
+        return isValid;
+
+    }
+}
