832848b440c6322439a866f8c19b2f6168ae2148
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-11-14 04:14:13 +0300

    File persistence API project added with a basic interface
    A project added to hold the classes to work with persisting objects in the files, and basic interface to work with this functionality added

---
+++ FilePersistence.API/IFilePersistence.cs
@@ -0,0 +1,15 @@
+﻿namespace FilePersistence.API;
+
+public interface IFilePersistence<TId, TEntityType>
+{
+    void Create(TEntityType entity);
+
+    TEntityType ReadById(TId id);
+
+    void Update(TId id);
+
+    void Delete(TId id);
+
+    IEnumerable<TEntityType> GetAll();
+
+}
