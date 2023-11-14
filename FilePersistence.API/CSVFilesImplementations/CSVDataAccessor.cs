9fc022d8c89a4db64fecd2e60f45b6d51ebda307
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-11-14 13:03:40 +0300

    A class to access the data in the CSV files added

---
+++ FilePersistence.API/CSVFilesImplementations/CSVDataAccessor.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+namespace FilePersistence.API.CSVFilesImplementations;
+
+public class CSVDataAccessor<TEntityType>
+{
+    private string _filePath;
+
+    public CSVDataAccessor(string path)
+    {
+        this._filePath = path;
+    }
+    
+    public void WriteDataToCsv(IEnumerable<TEntityType> data)
+    {
+        using var writer = new StreamWriter(this._filePath);
+        using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+        csv.WriteRecords(data);
+    }
+
+    public IEnumerable<TEntityType> ReadDataFromCsv()
+    {
+        using var reader = new StreamReader(this._filePath);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        return csv.GetRecords<TEntityType>().ToList();
+    }
+}
