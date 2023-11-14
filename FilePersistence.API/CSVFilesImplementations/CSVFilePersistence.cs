c31938746269e6d37b07603b58ce9ba9e68a3cfa
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-11-14 13:02:57 +0300

    A concrete class for the CSV files persistence added

---
+++ FilePersistence.API/CSVFilesImplementations/CSVFilePersistence.cs
@@ -0,0 +1,37 @@
+﻿using FilePersistence.API.FilesTypesInterfaces;
+
+namespace FilePersistence.API.CSVFilesImplementations;
+
+public class CSVFilePersistence<TId, TEntityType> : ICSVFilePersistence<TId, TEntityType>
+{
+    private string _filePath;
+
+    public CSVFilePersistence(string path)
+    {
+        this._filePath = path;
+    }
+    public void Create(TEntityType entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TEntityType ReadById(TId id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(TId id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(TId id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TEntityType> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
