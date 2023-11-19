5e6a4b7147eccec345a5d5ed94bf84ade2ed3e83
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-11-20 01:48:59 +0300

    method to get validation errors added to the data modeling library

--- DataModeling.Library/CustomBaseModel.cs
+++ DataModeling.Library/CustomBaseModel.cs
@@ -6,6 +6,14 @@ public abstract class CustomBaseModel
 {
     public Guid Id { get; init; }
 
+    public IEnumerable<ValidationResult> GetValidationErrors()
+    {
+        var context = new ValidationContext(this, serviceProvider: null, items: null);
+        var results = new List<ValidationResult>();
+        Validator.TryValidateObject(this, context, results, true);
+        return results;
+
+    }
     public bool Validate()
     {
         var context = new ValidationContext(this, serviceProvider: null, items: null);
